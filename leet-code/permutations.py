765644542c26f215f74c7aa385f9d0bbe3343fea
heojeongbo <heojungbo@gmail.com>
2021-12-12 15:35:41 +0900

    leetcode #46

---
+++ leet-code/permutations.py
@@ -0,0 +1,46 @@
+from typing import List
+import copy
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+
+        def recursive_dfs(elements, next, prev=[]):
+            if not next:
+                results.append(prev)
+                return
+
+            for e in next:
+                next_elements = copy.deepcopy(next)
+                next_elements.remove(e)
+
+                prev_elements = copy.deepcopy(prev)
+                prev_elements.append(e)
+
+                recursive_dfs(elements, next_elements, prev_elements)
+
+        def iterative_dfs():
+            stack = [([], nums)]
+
+            while stack:
+                prev, next = stack.pop()
+                if not next:
+                    results.append(prev)
+
+                for e in next:
+                    prev_elements = copy.deepcopy(prev)
+                    prev_elements.append(e)
+
+                    next_elements = copy.deepcopy(next)
+                    next_elements.remove(e)
+
+                    stack.append((prev_elements, next_elements))
+
+        # recursive_dfs(nums, nums)
+        iterative_dfs()
+        return results
+
+
+a = Solution()
+print(a.permute([1, 2, 3]))
